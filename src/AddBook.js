8a7db883923df9093a18519a3696cc298905268f
Arjun Thippeswamy <ht.arjun@outlook.com>
2019-01-26 22:15:43 +0000

    Create AddBook component to search for a book using BooksAPI

---
+++ src/AddBook.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
+
+class AddBook extends Component {
+  state = {
+    searchText: '',
+    books: []
+  }
+
+  searchBook = (event) => {
+    /*
+      NOTES: The search from BooksAPI is limited to a particular set of search terms.
+      You can find these search terms here:
+      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+      you don't find a specific author or title. Every search is limited by search terms.
+    */
+    
+    this.setState({
+      searchText: event.target.value
+    })
+    
+    BooksAPI.search(this.state.searchText, 20)
+      .then((books) => {
+        this.setState({
+          books
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a className="close-search">Close</a>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.searchText}
+              onChange={this.searchBook}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.books.map((book) => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.authors}</div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddBook
